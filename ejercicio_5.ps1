103db3b759781022ffa38d5eb6b4916b700f83b5
DavEm-afk <david.rangelt@uanl.edu.mx>
2025-08-16 21:52:12 -0600

    Ejercicio 5
    Validación de la existencia de un archivo.

---
+++ ejercicio_5.ps1
@@ -0,0 +1,31 @@
+﻿function Validar-Archivo {
+    param([string] $Ruta)
+
+    try {
+        
+        if (Test-Path $Ruta) {
+            $contenido = Get-Content $Ruta -ErrorAction Stop
+            return "Archivo encontrado y accesible: $Ruta"
+
+        } else {
+            throw "El archivo no existe."
+
+        }
+    }
+
+    catch {
+        return "Error: $_"
+
+    }
+
+    finally {
+        Write-Host "Validación finalizada para: $Ruta" -ForegroundColor Cyan
+
+    }
+}
+
+#Probar funcion
+
+Validar-Archivo -Ruta "C:\archivo_inexistente.txt"
+
+Validar-Archivo -Ruta "$env:USERPROFILE\Desktop\archivo.txt"
